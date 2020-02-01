1866e48e8653ba7fcfa0dbd3b74b39c24ea46731
uci-mars <chheangm@uci.edu>
2020-01-31 20:44:11 -0800

    Moved the client folder to be serve by express app

---
+++ routes/client/build/precache-manifest.ba84def7383bdc078dd7e00544c757ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5d8a23a533992c65961dd3434f5abf9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9987d9befa91f1498171",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "163dd15cca523a63d899",
+    "url": "/static/js/2.970da5d1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.970da5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9987d9befa91f1498171",
+    "url": "/static/js/main.ab6f013e.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
