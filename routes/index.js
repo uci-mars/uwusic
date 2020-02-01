7b83482562c2f03e587dd5db64198d85ec5c1ceb
uci-mars <chheangm@uci.edu>
2020-01-31 20:53:36 -0800

    i gave up using git cli

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 module.exports = router;
